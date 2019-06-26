f2f14a6b38e7bbc7ced84980743ad74569449101
Bruno Casotto <casottoalves@hotmail.com>
2019-06-26 14:32:28 -0300

    resolve static paths

--- entry/server.js
+++ entry/server.js
@@ -2,14 +2,16 @@ import { createApp } from '../resources/app'
 
 export default context => new Promise((resolve, reject) => {
   const { app, router } = createApp()
-  router.push(context.url)
+  const { url } = context
 
+  router.push(url)
   router.onReady(() => {
     const matchedComponents = router.getMatchedComponents()
+
     if (!matchedComponents.length) {
-      reject({ code: 404 })
-    } else {
-      resolve(app)
+      return reject({ code: 404 })
     }
+
+    return resolve(app)
   }, reject)
 })
--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const path = require('path')
+const app = express()
 const port = process.env.PORT || 8080
+
+const staticPath = path.resolve(__dirname, 'dist')
+app.use('/static', express.static(staticPath))
 
 const MainController = require('./src/controller')
 const controller = new MainController()
-
-app.use('/static', express.static(`${__dirname}/dist`))
 app.get('*', controller.render)
-app.listen(port, () => (console.log(`Example app listening on ${port}!`)))
+
+app.listen(
+  port,
+  () => {
+    console.log(`Example app listening on ${port}!`)
+  }
+)
