fe5770bca01cfe45056316be70a29e69c569d9aa
Bruno Casotto <casottoalves@hotmail.com>
2019-06-29 10:00:50 -0300

    sync router done

--- resources/app.js
+++ resources/app.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import { sync } from 'vuex-router-sync'
 import Main from './Main.vue'
 import { createStore } from './store'
 import { createRouter } from './router'
@@ -6,6 +7,8 @@ import { createRouter } from './router'
 export const createApp = () => {
   const store = createStore()
   const router = createRouter()
+
+  sync(store, router)
 
   const app = new Vue({
     router,
